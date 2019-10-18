2baf79a832ec96f50b971791ab3b203a3aa6f9bc
selvanayagam06598 <mselva1993@gmail.com>
2019-10-18 12:43:08 +0530

    name

---
+++ src/org/bike/Bike.java
@@ -0,0 +1,12 @@
+package org.bike;
+
+public class Bike {
+	public void cost() {
+		// TODO Auto-generated method stub
+System.out.println("bike cost is 50000");
+	}
+	public void speed() {
+		// TODO Auto-generated method stub
+System.out.println("top speed is 120km");
+	}
+}
---
+++ src/org/bike/Ktm.java
@@ -0,0 +1,17 @@
+package org.bike;
+
+public class Ktm extends Bike {
+	public void cost() {
+		// TODO Auto-generated method stub
+System.out.println("cost is 100000");
+	}
+	public void speed() {
+		// TODO Auto-generated method stub
+System.out.println("speed is 130km");
+	}
+	public static void main(String[] args) {
+		Ktm r=new Ktm();
+		r.cost();
+		r.speed();
+	}
+}
